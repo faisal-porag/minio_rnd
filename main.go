a1a8d05e722afe346f15ad2858229e40dba7c333
Faisal Porag <faisal.porag@upaybd.com>
2022-06-20 12:13:43 +0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	endpoint := "play.min.io"
+	accessKeyID := "Q3AM3UQ867SPQQA43P2F"
+	secretAccessKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+	//useSSL := true
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: true,
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("minio client initiate error!")
+	}
+
+	log.Info().Msg("Successfully initiated.")
+	log.Info().Msgf("Result: %#v\n", minioClient) // minioClient is now setup
+}
